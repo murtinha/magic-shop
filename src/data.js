ab2f71afdffeb59fc77ed5b82bebf46f552a36e0
Eric Murta <ericmurta@hotmail.com>
2019-02-10 12:56:03 -0300

    Add data mock file

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const card = {
+  image: 'path/imagem.png',
+  id: 1,
+  name: 'Black Lotus',
+  manaCost: '0',
+  type: 'Artifact',
+  rarity: 'Rare',
+  setName: 'Magic collection',
+  text: 'Card text',
+  flavor: 'Card flavor',
+  artist: 'murtinha',
+};
+
+export const cards = [
+  {
+    image: 'path/imagem.png',
+    name: 'Black Lotus',
+    id: 1,
+    manaCost: '0',
+    type: 'Artifact',
+    rarity: 'Rare',
+    setName: 'Magic collection',
+    text: 'Card text',
+    flavor: 'Card flavor',
+    artist: 'murtinha',
+  },
+  {
+    image: 'path/imagem2.png',
+    name: 'Bufao',
+    id: 2,
+    manaCost: 'B2G',
+    type: 'Creature',
+    rarity: 'Legend',
+    setName: 'Magic collection 2',
+    text: 'Card text 2',
+    flavor: 'Card flavor 2',
+    artist: 'murtinha2',
+  }
+];
+
+export const deck = [
+  {
+    name: 'Black Lotus',
+    cardId: 1,
+  },
+  {
+    name: 'Bufao',
+    cardId: 2,
+  }
+];
