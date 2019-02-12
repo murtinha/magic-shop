20538f8b66466e1677fb0cc88af911d3ba66e4ce
murtinha <ericmurta@hotmail.com>
2019-02-12 15:49:14 -0300

    Add more cards to data

--- src/data.js
+++ src/data.js
@@ -35,6 +35,42 @@ export const cards = [
     text: 'Card text 2',
     flavor: 'Card flavor 2',
     artist: 'murtinha2',
+  },
+  {
+    image: 'path/imagem3.png',
+    name: 'Forest',
+    id: 3,
+    manaCost: '',
+    type: 'Land',
+    rarity: 'Basic',
+    setName: 'Magic collection 3',
+    text: 'Card text 3',
+    flavor: 'Card flavor 3',
+    artist: 'murtinha3',
+  },
+  {
+    image: 'path/imagem4.png',
+    name: 'Swamp',
+    id: 4,
+    manaCost: '',
+    type: 'Land',
+    rarity: 'Basic',
+    setName: 'Magic collection 4',
+    text: 'Card text 4',
+    flavor: 'Card flavor 4',
+    artist: 'murtinha4',
+  },
+  {
+    image: 'path/imagem5.png',
+    name: 'Island',
+    id: 5,
+    manaCost: '',
+    type: 'Land',
+    rarity: 'Basic',
+    setName: 'Magic collection 5',
+    text: 'Card text 5',
+    flavor: 'Card flavor 5',
+    artist: 'murtinha5',
   }
 ];
 
@@ -44,7 +80,15 @@ export const deck = [
     cardId: 1,
   },
   {
-    name: 'Bufao',
+    name: 'Forest',
     cardId: 2,
+  },
+  {
+    name: 'Swamp',
+    cardId: 3,
+  },
+  {
+    name: 'Island',
+    cardId: 4,
   }
 ];
